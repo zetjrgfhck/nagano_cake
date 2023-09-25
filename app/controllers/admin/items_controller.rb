4b908b48d6f4534f49c048abf3f8b39f60bea5af
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2023-09-25 13:12:56 +0000

    Initial Commit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,33 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @item = Item.new
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    #アイテム　ストロングパラメータで受けとったふぉーむの情報
+    #情報の保存
+    #遷移先かく
+    
   end
 
   def show
+    
   end
 
   def edit
+  
   end
 
   def update
+  
   end
+ 
+ def item_params
+    #params.require(:item).permit(カラム)
+  end
+
 end
