ad3a9bc8f34643f65cff211347df50b459fcf496
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2023-10-06 15:48:38 +0000

    Initial Commit

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,17 +6,26 @@ class Public::OrdersController < ApplicationController
 
   def new
   end
-  
+
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
-  
+
   def create
   end
 
   def confirm
   end
-  
+
   def thanks
+  end
+
+
+ private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name, :payment_method)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
 
 belongs_to :customer, optional: true
 has_many :order_detail
+enum payment_method: { credit_card: 0, transfer: 1 }
 end
